fd409f8a72e76bd5fe61a21aa9a1d7889660c7a0
Poncho <jplyles1212@gmail.com>
2021-01-31 19:09:55 -0800

    Create recursive function to find factorial of number

--- recursion.py
+++ recursion.py
@@ -7,16 +7,22 @@
 
 # countdown(10)
 
-def greet(name):
-    print("hello " + name + "!")
-    greet2(name)
-    print("getting ready to say bye...")
-    bye()
+# def greet(name):
+#     print("hello " + name + "!")
+#     greet2(name)
+#     print("getting ready to say bye...")
+#     bye()
 
-def greet2(name):
-    print("how are you, " + name + "?")
+# def greet2(name):
+#     print("how are you, " + name + "?")
 
-def bye():
-    print("ok bye!")
+# def bye():
+#     print("ok bye!")
 
-greet("Poncho")
+# greet("maggie")
+
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x - 1)
