84ad7be6498f32d088235b65140528f8cedbfaa3
Poncho <jplyles1212@gmail.com>
2021-02-01 15:18:40 -0800

    Create recursive function to find max number in list

--- quickSort.py
+++ quickSort.py
@@ -13,9 +13,17 @@
 
 # print(sum([2, 4, 6]))
 
-def count(list):
-    if list == []:
-        return 0
-    return 1 + count(list[1:])
+# def count(list):
+#     if list == []:
+#         return 0
+#     return 1 + count(list[1:])
 
-print(count([1, 8, 4, 56, 5, 12]))
+# print(count([1, 8, 4, 56, 5, 12]))
+
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
+
+print(max([1, 8, 4, 56, 5, 12]))
