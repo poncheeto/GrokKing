cf3d66d7b763e9834174955ebdfe89b9620e01cb
Poncho <jplyles1212@gmail.com>
2021-02-01 14:57:54 -0800

    Create recursive function to count number of elements in list

--- quickSort.py
+++ quickSort.py
@@ -6,9 +6,16 @@
 
 # print(sum([1, 2, 3, 4]))
 
-def sum(list):
+# def sum(list):
+#     if list == []:
+#         return 0
+#     return list[0] + sum(list[1:])
+
+# print(sum([2, 4, 6]))
+
+def count(list):
     if list == []:
         return 0
-    return list[0] + sum(list[1:])
+    return 1 + count(list[1:])
 
-print(sum([2, 4, 6]))
+print(count([1, 8, 4, 56, 5, 12]))
