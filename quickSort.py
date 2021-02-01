c4eb76fa2fb0d187bcedb28ac7da5839289e80b8
Poncho <jplyles1212@gmail.com>
2021-01-31 20:01:15 -0800

    Create recursive function to find sum of all numbers in list

--- quickSort.py
+++ quickSort.py
@@ -1,7 +1,14 @@
-def sum(arr):
-    total = 0
-    for x in arr:
-        total += x
-    return total
+# def sum(arr):
+#     total = 0
+#     for x in arr:
+#         total += x
+#     return total
 
-print(sum([1, 2, 3, 4]))
+# print(sum([1, 2, 3, 4]))
+
+def sum(list):
+    if list == []:
+        return 0
+    return list[0] + sum(list[1:])
+
+print(sum([2, 4, 6]))
