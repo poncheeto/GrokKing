bfcd3aaeee857551ba532df18e7af93ccd0fd00b
Poncho <jplyles1212@gmail.com>
2021-01-31 18:53:11 -0800

    Demonstrate using call stack

--- recursion.py
+++ recursion.py
@@ -1,8 +1,22 @@
-def countdown(i):
-    print(i)
-    if i <= 1:
-        return
-    else:
-        countdown(i - 1)
+# def countdown(i):
+#     print(i)
+#     if i <= 1:
+#         return
+#     else:
+#         countdown(i - 1)
 
-countdown(10)
+# countdown(10)
+
+def greet(name):
+    print("hello " + name + "!")
+    greet2(name)
+    print("getting ready to say bye...")
+    bye()
+
+def greet2(name):
+    print("how are you, " + name + "?")
+
+def bye():
+    print("ok bye!")
+
+greet("Poncho")
