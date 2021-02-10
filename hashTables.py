322280f54aeb6f2a8f394323e50de54c9c30d8dc
Poncho <jplyles1212@gmail.com>
2021-02-09 18:40:14 -0800

    Create hash function to check whether someone has voted

--- hashTables.py
+++ hashTables.py
@@ -6,8 +6,21 @@
 # print(book)
 # print(book["avocado"])
 
-phone_book = {}
+# phone_book = {}
 
-phone_book["jenny"] = 8675309
-phone_book["emergency"] = 911
-print(phone_book["jenny"])
+# phone_book["jenny"] = 8675309
+# phone_book["emergency"] = 911
+# print(phone_book["jenny"])
+
+voted = {}
+
+def check_voter(name):
+    if voted.get(name):
+        print("kick them out!")
+    else:
+        voted[name]= True
+        print("let them vote!")
+
+check_voter('tom')
+check_voter('mike')
+check_voter('mike')
