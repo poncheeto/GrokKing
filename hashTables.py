002d1f6fe498318c1d47194320d8abe3df227520
Poncho <jplyles1212@gmail.com>
2021-02-09 18:34:49 -0800

    Create hash table for phone numbers

--- hashTables.py
+++ hashTables.py
@@ -1,7 +1,13 @@
-book = dict()
+# book = dict()
 
-book["apple"] = 0.67
-book["milk"] = 1.49
-book["avocado"] = 1.49
-print(book)
-print(book["avocado"])
+# book["apple"] = 0.67
+# book["milk"] = 1.49
+# book["avocado"] = 1.49
+# print(book)
+# print(book["avocado"])
+
+phone_book = {}
+
+phone_book["jenny"] = 8675309
+phone_book["emergency"] = 911
+print(phone_book["jenny"])
